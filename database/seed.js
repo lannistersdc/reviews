cf6573334e59bc2d497f58e2eb0abcf7b81c7502
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-04-19 09:33:01 -0700

    Database seed function working

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose');
-const Review = require('./index.js');
+// const mongoose = require("mongoose");
+// const Review = require("./index.js");
+const fs = require("fs");
+// const {
+//   Readable
+// } = require("stream");
+const file = fs.createWriteStream("./seed.csv");
 
 let seedData = [];
 
@@ -23,214 +28,216 @@ var normalRand = (min, max, skew) => {
 };
 
 var firstName = [
-  'Liezel',
-  'Olivia',
-  'Ava',
-  'Isabella',
-  'Sophia',
-  'Mia',
-  'Charlotte',
-  'Amelia',
-  'Evelyn',
-  'Abigail',
-  'Harper',
-  'Emily',
-  'Elizabeth',
-  'Avery',
-  'Sofia',
-  'Ella',
-  'Madison',
-  'Scarlett',
-  'Victoria',
-  'Aria',
-  'Grace',
-  'Chloe',
-  'Camila',
-  'Penelope',
-  'Riley',
-  'Jeff',
-  'Uttej',
-  'William',
-  'James',
-  'Logan',
-  'Benjamin',
-  'Mason',
-  'Elijah',
-  'Oliver',
-  'Jacob',
-  'Lucas',
-  'Michael',
-  'Alexander',
-  'Ethan',
-  'Daniel',
-  'Matthew',
-  'Aiden',
-  'Henry',
-  'Joseph',
-  'Jackson',
-  'Samuel',
-  'Sebastian',
-  'David',
-  'Carter',
-  'Wyatt'
+  "Liezel",
+  "Olivia",
+  "Ava",
+  "Isabella",
+  "Sophia",
+  "Mia",
+  "Charlotte",
+  "Amelia",
+  "Evelyn",
+  "Abigail",
+  "Harper",
+  "Emily",
+  "Elizabeth",
+  "Avery",
+  "Sofia",
+  "Ella",
+  "Madison",
+  "Scarlett",
+  "Victoria",
+  "Aria",
+  "Grace",
+  "Chloe",
+  "Camila",
+  "Penelope",
+  "Riley",
+  "Jeff",
+  "Uttej",
+  "William",
+  "James",
+  "Logan",
+  "Benjamin",
+  "Mason",
+  "Elijah",
+  "Oliver",
+  "Jacob",
+  "Lucas",
+  "Michael",
+  "Alexander",
+  "Ethan",
+  "Daniel",
+  "Matthew",
+  "Aiden",
+  "Henry",
+  "Joseph",
+  "Jackson",
+  "Samuel",
+  "Sebastian",
+  "David",
+  "Carter",
+  "Wyatt"
 ];
 
 var lastName = [
-  'Jardine',
-  'Gujral',
-  'Manalo',
-  'Su',
-  'Brown',
-  'Davis',
-  'Miller',
-  'Wilson',
-  'Moore',
-  'Taylor',
-  'Anderson',
-  'Thomas',
-  'Jackson',
-  'White',
-  'Harris',
-  'Martin',
-  'Thompson',
-  'Garcia',
-  'Martinez',
-  'Robinson',
-  'Clark',
-  'Rodriguez',
-  'Lewis',
-  'Lee',
-  'Walker',
-  'Hall',
-  'Allen',
-  'Young',
-  'Hernandez',
-  'King',
-  'Wright',
-  'Lopez',
-  'Hill',
-  'Scott',
-  'Green',
-  'Adams',
-  'Baker',
-  'Gonzalez',
-  'Nelson',
-  'Carter',
-  'Mitchell',
-  'Perez',
-  'Roberts',
-  'Turner',
-  'Phillips',
-  'Campbell',
-  'Parker',
-  'Evans',
-  'Edwards',
-  'Collins'
+  "Jardine",
+  "Gujral",
+  "Manalo",
+  "Su",
+  "Brown",
+  "Davis",
+  "Miller",
+  "Wilson",
+  "Moore",
+  "Taylor",
+  "Anderson",
+  "Thomas",
+  "Jackson",
+  "White",
+  "Harris",
+  "Martin",
+  "Thompson",
+  "Garcia",
+  "Martinez",
+  "Robinson",
+  "Clark",
+  "Rodriguez",
+  "Lewis",
+  "Lee",
+  "Walker",
+  "Hall",
+  "Allen",
+  "Young",
+  "Hernandez",
+  "King",
+  "Wright",
+  "Lopez",
+  "Hill",
+  "Scott",
+  "Green",
+  "Adams",
+  "Baker",
+  "Gonzalez",
+  "Nelson",
+  "Carter",
+  "Mitchell",
+  "Perez",
+  "Roberts",
+  "Turner",
+  "Phillips",
+  "Campbell",
+  "Parker",
+  "Evans",
+  "Edwards",
+  "Collins"
 ];
 
 var places = [
-  'Los Angeles',
-  'San Francisco',
-  'Sacramento',
-  'San Diego',
-  'Fresno',
-  'Long Beach',
-  'New York'
+  "Los Angeles",
+  "San Francisco",
+  "Sacramento",
+  "San Diego",
+  "Fresno",
+  "Long Beach",
+  "New York"
 ];
 
 var opening = [
-  'just',
-  '',
-  '',
-  '',
-  '',
-  'went with family,',
-  'went with girlfriend,',
-  'went with boyfriend,',
-  'totally',
-  'heard about this place,',
-  'last night i',
-  'i',
-  'the guy next to me',
-  'great date spot,',
-  'ordered the special,'
+  "just",
+  "",
+  "",
+  "",
+  "",
+  "went with family,",
+  "went with girlfriend,",
+  "went with boyfriend,",
+  "totally",
+  "heard about this place,",
+  "last night i",
+  "i",
+  "the guy next to me",
+  "great date spot,",
+  "ordered the special,"
 ];
 
 var verbs = [
-  'ate',
-  'dined',
-  'enjoyed',
-  'devoured',
-  'noshed',
-  'consumed',
-  'experienced',
-  'ordered',
-  'got',
-  'inhaled',
-  'nibbled',
-  'slurped',
-  'shooped',
-  'nommed'
+  "ate",
+  "dined",
+  "enjoyed",
+  "devoured",
+  "noshed",
+  "consumed",
+  "experienced",
+  "ordered",
+  "got",
+  "inhaled",
+  "nibbled",
+  "slurped",
+  "shooped",
+  "nommed"
 ];
 
 var objects = [
-  'my',
-  'some',
-  'the',
-  'a',
-  'my',
-  'an entire',
-  'this',
-  'that',
-  'the',
-  'the big',
-  'a new style of'
+  "my",
+  "some",
+  "the",
+  "a",
+  "my",
+  "an entire",
+  "this",
+  "that",
+  "the",
+  "the big",
+  "a new style of"
 ];
 
 var nouns = [
-  'cat',
-  'pizza',
-  'pasta',
-  'sushi roll',
-  'plate of nachos',
-  'taco',
-  'potato',
-  'crumpet',
-  'ice cream',
-  'pancake',
-  'waffle',
-  'burger',
-  'plate of fries',
-  'buffalo wings',
-  'dim sum',
-  'curry'
+  "cat",
+  "pizza",
+  "pasta",
+  "sushi roll",
+  "plate of nachos",
+  "taco",
+  "potato",
+  "crumpet",
+  "ice cream",
+  "pancake",
+  "waffle",
+  "burger",
+  "plate of fries",
+  "buffalo wings",
+  "dim sum",
+  "curry"
 ];
 
 var tags = [
-  '#thuglife',
-  '#jank',
-  '#gucci',
-  'would recommend',
-  'for real',
-  '#notanad',
-  '#ballin',
-  '#omg',
-  '#yolo',
-  '#delicious',
-  '',
-  '',
-  '',
-  ''
+  "#thuglife",
+  "#jank",
+  "#gucci",
+  "would recommend",
+  "for real",
+  "#notanad",
+  "#ballin",
+  "#omg",
+  "#yolo",
+  "#delicious",
+  "",
+  "",
+  "",
+  ""
 ];
 
-for (let i = 1; i <= 100; i++) {
+var id = 1;
+
+const oneReview = () => {
   let reviewCount = Math.floor(Math.random() * 500) + 1;
   let skew = Math.random() * 1.464973;
   for (let j = 0; j < reviewCount; j++) {
     let review = {
       restaurantID: 0,
-      username: '',
-      location: '',
+      username: "",
+      location: "",
       vip: false,
       totalReviews: 0,
       overall: 0,
@@ -240,13 +247,14 @@ for (let i = 1; i <= 100; i++) {
       value: 0,
       recommend: false,
       date: new Date(),
-      text: ''
+      text: ""
     };
 
-    review.restaurantID = i;
+    review.restaurantID = id;
+    id++;
 
     review.username = [randomElement(firstName), randomElement(lastName)].join(
-      ''
+      ""
     );
 
     review.location = randomElement(places);
@@ -284,23 +292,47 @@ for (let i = 1; i <= 100; i++) {
       randomElement(objects),
       randomElement(nouns),
       randomElement(tags)
-    ].join(' ');
+    ].join(" ");
 
-    if (review.text.charAt(0) === ' ') {
+    if (review.text.charAt(0) === " ") {
       review.text.slice(1, review.text.length);
     }
 
-    seedData.push(review);
+    return review;
+  }
+};
+
+function streamFunc(writer, data, encoding, callback) {
+  let x = 1e7;
+  write();
+
+  function write() {
+    let ok = true;
+    do {
+      if (x % 100000 === 0) {
+        console.log(x + ' items remaining.');
+      }
+      if (x === 1e7) {
+        writer.write('restaurantID, username, location, vip, totalReviews, overall, food, service, ambience, value, recommend, date, text\n')
+      }
+      x--;
+      if (x === 0) {
+        // last time!
+        var str = Object.values(oneReview()).join(',');
+        writer.write(str, encoding, callback);
+      } else {
+        // See if we should continue, or wait.
+        // Don't pass the callback, because we're not done yet.
+        var str = Object.values(oneReview()).join(',') + '\n';
+        ok = writer.write(str, encoding);
+      }
+    } while (x > 0 && ok);
+    if (x > 0) {
+      // had to stop early!
+      // write some more once it drains
+      writer.once("drain", write);
+    }
   }
 }
 
-const seedFunction = () => {
-  Review.create(seedData)
-    .then(() => {
-      console.log('Database seeded');
-      mongoose.connection.close();
-    })
-    .catch(error => console.error(error));
-};
-
-seedFunction();
+streamFunc(file);
