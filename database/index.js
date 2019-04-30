1e2bf8a518cdf8c8fd117e9a36df600396e3028f
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-04-30 09:36:18 -0700

    Database | updated database files to connect to remote instance

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect('mongodb://localhost/review', {
+  .connect('mongodb://adam:killme@3.16.129.244/review', {
     useNewUrlParser: true
   })
   .then(() => console.log('Connected to mongoDB'));
