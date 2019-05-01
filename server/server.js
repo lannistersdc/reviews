06a7497929e249dce9aeaaac00f1448e79311215
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-05-01 10:36:13 -0700

    Update to loader route

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public/')));
 app.use('/api', router);
-app.use('/loaderio-a6f557959091a23acfbd01b7a8f33a57', (req, res) => {
-    res.status(200).send('loaderio-a6f557959091a23acfbd01b7a8f33a57');
+app.use('/loaderio-310e0fa00ddae327d9f06de1023bc6d5', (req, res) => {
+    res.status(200).send('loaderio-310e0fa00ddae327d9f06de1023bc6d5');
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
